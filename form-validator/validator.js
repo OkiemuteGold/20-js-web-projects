e7f4c3472863e1c58694e4c13dec1770f465dcd6
Okiemute Gold <goldokiemute@gmail.com>
2021-06-23 23:47:58 +0100

    validation username and password length, and for email

--- form-validator/validator.js
+++ form-validator/validator.js
@@ -19,25 +19,55 @@ function showSuccess(input) {
 }
 
 // Check email validity
-function isValidEmail(email) {
-    const re =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-9-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLocaleLowerCase());
-}
+// function isValidEmail(email) {
+//     const re =
+//         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-9-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLocaleLowerCase());
+// }
 
+// Check required for all fields
 function checkRequired(inputArray) {
     inputArray.forEach(function (input) {
         console.log(input.id);
         if (input.value.trim() === "") {
-            showError(input, `${capitalizeFieldName(input)} is required`);
+            showError(input, `${getFieldName(input)} is required`);
         } else {
             showSuccess(input);
         }
     });
 }
 
-// Capitalize first letter of field name
-function capitalizeFieldName(input) {
+// Check password and username length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check email validity
+function checkEmail(input) {
+    const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-9-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
+}
+
+// Get field name and Capitalize first letter
+function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
@@ -46,4 +76,8 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 20);
+    checkEmail(email);
 });
